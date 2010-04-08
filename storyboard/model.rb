d03042e247510178d5bb123b4fa83841a0f14580
Oliver Steele <steele@osteele.com>
2010-04-08 10:23:34 -0400

    add graphics context as panel parameter

--- storyboard/model.rb
+++ storyboard/model.rb
@@ -43,7 +43,7 @@ module Storyboard
     def run(sketch, runner, time)
       unless @called
         @called = true
-        context(runner).instance_eval &@block
+        create_context(sketch, runner).instance_eval &@block
         # Display this after invoking the block, since the blocks sets
         # the caption
         puts "#{self.name}" + (self.caption ? ": #{self.caption}" : '')
@@ -60,9 +60,9 @@ module Storyboard
       @called = false
     end
 
-    def context(runner)
+    def create_context(sketch, runner)
       scene.class.class_eval do
-        attr_accessor :build_panel, :stage
+        attr_accessor :build_panel, :stage, :g
       end
       scene.instance_eval do
         def caption(*args); @build_panel.caption(*args); end
@@ -72,6 +72,7 @@ module Storyboard
       end
       scene.build_panel = self
       scene.stage = runner.stage_manager
+      scene.g = sketch
       scene
     end
 
@@ -86,7 +87,7 @@ module Storyboard
 
     def avar(start=1.0, stop=nil, &block)
       if start.instance_of?(Array)
-        avar = ArrayAvar.new(start, stop)
+        avar = ColorAvar.new(start, stop)
       else
         start, stop = 0.0, start unless stop
         avar = AVar.new(start, stop, &block)
@@ -161,6 +162,38 @@ module Storyboard
     end
   end
 
+  class ColorAvar
+    attr_accessor :s
+
+    def initialize(start, stop)
+      @start, @stop = start, stop
+      @rgb_start = hsb2rgb(*start)
+      @rgb_stop = hsb2rgb(*stop)
+      #p [@start, @rgb_start]
+      @s = 0
+    end
+
+    def to_a
+      #@start.zip(@stop).map { |a, b| a + AVar.ease(s) * (b - a) }
+      rgb = @rgb_start.zip(@rgb_stop).map { |a, b| a + AVar.ease(s) * (b - a) }
+      #rgb = [0.0, 0.0, 1.0]
+      rgb2hsb(*rgb)
+    end
+
+    def rgb2hsb(r, g, b)
+      import "java.awt.Color"
+      # p [r,g,b]
+      r, g, b = [r, g, b].map { |c| (255 * c).to_i }
+      java.awt.Color.RGBtoHSB(r, g, b, nil).to_a
+    end
+
+    def hsb2rgb(h, s, b)
+      import "java.awt.Color"
+      color = java.awt.Color.getHSBColor(h, s, b)
+      return [:getRed, :getGreen, :getBlue].map { |m| color.send(m) / 255.0 }
+    end
+  end
+
   class DisplaySettings
     attr_accessor :size, :scale, :color_mode, :background
 
@@ -175,6 +208,7 @@ module Storyboard
       sketch.background *background if background
       sketch.scale(*scale) if scale
       sketch.smooth
+      sketch.stroke_weight 2
     end
   end
 end
