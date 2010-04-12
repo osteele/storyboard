d4f35665953e56404586af16b8ee9c92e9de98d0
Oliver Steele <steele@osteele.com>
2010-04-12 12:03:24 -0400

    add bin/storyboard, that loads file via processing_loader.rb

---
+++ processing_loader.rb
@@ -0,0 +1,8 @@
+def setup; end # this tells the Processing runner to place methods in the Sketch class
+
+require 'watch_require'
+require 'graphics-utils'
+require 'storyboard'
+
+dft_panels = ARGV[0]
+require dft_panels
